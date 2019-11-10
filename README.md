a0c825fda2c2e079f649ec190d375e00c1483733
Edwin García <spark.com.co@gmail.com>
2019-11-10 15:54:42 -0500

    Creación de proyecto Google API para usar google OAuth e implementación de sign provider

--- README.md
+++ README.md
@@ -17,3 +17,12 @@ Corre la aplicación en modo producción
 
 ## Notas
 
+### Creación de proyecto en Google api para hacer autenticación en OAuth2.0
+
+1. Vamos a la [Consola de APIs  y servicios](https://console.developers.google.com) de Google
+2. Creamos un proyecto
+3. En el menú lateral izquierdo seleccionamos **Pantalla de consentimiento** y en **Nombre de la aplicación** asignamos un nombre que en este caso será **Platzi videos**
+4. En el menú lateral izquierdo seleccionamos **Credendials > Create credentials > OAuth client ID**
+5. Nos aseguramos de elegir **Web application** como tipo de aplicación
+6. Luego establecemos el nombre del cliente que en nuestro caso será **SSR server**, el **Authorized JavaScript Origins:** _http://localhost:8000_ y el **Authorized redirect URIs:** _http://localhost:8000/auth/google-oauth/callback_, después se cambiarán los valores en producción.
+7. Al final copiamos **Client ID** y **Client secret** y los usamos en nuestro entorno como **GOOGLE_CLIENT_ID** y **GOOGLE_CLIENT_SECRET**
--- index.js
+++ index.js
@@ -16,6 +16,7 @@ require('./utils/auth/strategies/basic')
 
 app.post("/auth/sign-in", async function(req, res, next) {
   passport.authenticate('basic', (err, data) => {
+    const { token, user } = data
     try{
       if(err || !data){
         next(Boom.unauthorized())
@@ -55,11 +56,40 @@ app.get("/movies", async function(req, res, next) {
 });
 
 app.post("/user-movies", async function(req, res, next) {
-
+  try{
+    const { body: userMovie } = req
+    const { token } = req.cookies
+    const { data, status } = await axios({
+      url: `${config.apiUrl}/api/user-movies`,
+      headers: { Authorization: `Bearer ${token}` },
+      method: 'post',
+      data: userMovie
+    })
+    if(status !== 201){
+      return next(Boom.badImplementation())
+    }
+    res.status(201).json(data)
+  }catch(err){
+    next(err)
+  }
 });
 
 app.delete("/user-movies/:userMovieId", async function(req, res, next) {
-
+  try{
+    const { userMovieId } = req.params
+    const { token } = req.cookies
+    const { data, status } = await axios({
+      url: `${config.apiUrl}/api/user-movies/${userMovieId}`,
+      headers: { Authorization: `Bearer ${token}` },
+      method: 'delete'
+    })
+    if(status !== 200){
+      return next(Boom.badImplementation())
+    }
+    res.status(200).json(data)
+  }catch(err){
+    next(err)
+  }
 });
 
 app.listen(config.port, function() {
